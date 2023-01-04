bae24d283f14f37685d6596f8c8ac95c9af7eb99
LEESEUNGYEOL <lsy12210@gmail.com>
2023-01-04 23:25:53 +0900

    database connection setting

--- kcalog/src/main/java/com/example/kcalog/KcalogApplication.java
+++ kcalog/src/main/java/com/example/kcalog/KcalogApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.kcalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class KcalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		SpringApplication.run(KcalogApplication.class, args);
 
+	}
 }
